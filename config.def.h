e83f1720ad6a9a6e855f261fc4848e7cb24fb318
León Illanes <lillanes@cs.toronto.edu>
2019-07-15 23:15:15 -0400

    make layout changing with mouse clicks easier

--- config.def.h
+++ config.def.h
@@ -140,6 +140,7 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button2,        setlayout,      {.v = &layouts[1]} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },

3aeb5478831d67a5c78bea51321ab54ab09d89bc
León Illanes <lillanes@cs.toronto.edu>
2019-07-16 18:31:00 -0400

    make ranger windows float

--- config.def.h
+++ config.def.h
@@ -31,6 +31,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ NULL,       NULL,   "ranger",       0,            1,           -1 },
 };
 
 /* layout(s) */
